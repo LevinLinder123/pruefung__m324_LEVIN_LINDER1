a71516006bccbb217b00fb2a4712eaac0114c4a3
Levin Linder <levin06@bluewin.ch>
2024-09-19 10:59:35 +0200

    formatter test

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,44 @@
 /** @type {import('eslint').Linter.FlatConfig} */
-module.exports = [
+module.exports =
+  [
     {
-      languageOptions: {
-        globals: {
-          window: 'readonly',
-          document: 'readonly',
-          console: 'readonly'
+      languageOptions:
+        {
+          globals:
+            {
+              window:
+                'readonly',
+              document:
+                'readonly',
+              console:
+                'readonly',
+            },
+          parserOptions:
+            {
+              ecmaVersion: 2021,
+              sourceType:
+                'module',
+            },
         },
-        parserOptions: {
-          ecmaVersion: 2021,
-          sourceType: 'module'
-        }
-      },
-      plugins: {
-        // Falls du Plugins verwendest, füge sie hier hinzu
-      },
-      rules: {
-        'eqeqeq': ['error', 'always'],
-        'no-unused-vars': ['error'], // Zeigt Warnungen für nicht verwendete Variablen an
-        'prefer-const': ['error']  // Zeigt Fehler an, wenn `let` verwendet wird, obwohl die Variable nie neu zugewiesen wird
-      }
-    }
+      plugins:
+        {
+          // Falls du Plugins verwendest, füge sie hier hinzu
+        },
+      rules:
+        {
+          eqeqeq:
+            [
+              'error',
+              'always',
+            ],
+          'no-unused-vars':
+            [
+              'error',
+            ], // Zeigt Warnungen für nicht verwendete Variablen an
+          'prefer-const':
+            [
+              'error',
+            ], // Zeigt Fehler an, wenn `let` verwendet wird, obwohl die Variable nie neu zugewiesen wird
+        },
+    },
   ];
-  
--- index.js
+++ index.js
@@ -1,14 +1,30 @@
-console.log(chalk.blue("it works"));
+console.log(
+  chalk.blue(
+    'it works',
+  ),
+);
 var myUnsignedVariable = 5;
 
-function checkEquality(a, b) {
-    if (a === b) {
-      console.log('Values are equal');
-    } else {
-      console.log('Values are not equal');
-    }
+function checkEquality(
+  a,
+  b,
+) {
+  if (
+    a ===
+    b
+  ) {
+    console.log(
+      'Values are equal',
+    );
+  } else {
+    console.log(
+      'Values are not equal',
+    );
   }
-  
+}
 
 myUnsignedVariable++;
-checkEquality(5,myUnsignedVariable)
+checkEquality(
+  5,
+  myUnsignedVariable,
+);

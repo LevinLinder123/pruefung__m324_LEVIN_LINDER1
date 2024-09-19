40763df9429743b28e80dbc9c2015a5634d3a74a
Levin Linder <levin06@bluewin.ch>
2024-09-19 10:55:29 +0200

    linter test

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+/** @type {import('eslint').Linter.FlatConfig} */
+module.exports = [
+    {
+      languageOptions: {
+        globals: {
+          window: 'readonly',
+          document: 'readonly',
+          console: 'readonly'
+        },
+        parserOptions: {
+          ecmaVersion: 2021,
+          sourceType: 'module'
+        }
+      },
+      plugins: {
+        // Falls du Plugins verwendest, füge sie hier hinzu
+      },
+      rules: {
+        'eqeqeq': ['error', 'always'],
+        'no-unused-vars': ['error'], // Zeigt Warnungen für nicht verwendete Variablen an
+        'prefer-const': ['error']  // Zeigt Fehler an, wenn `let` verwendet wird, obwohl die Variable nie neu zugewiesen wird
+      }
+    }
+  ];
+  
--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 console.log(chalk.blue("it works"));
+var myUnsignedVariable = 5;
+
+function checkEquality(a, b) {
+    if (a == b) {
+      console.log('Values are equal');
+    } else {
+      console.log('Values are not equal');
+    }
+  }
+  

3f812471d325e7e2ccab26b4ad74a47ddce3df8e
Levin Linder <levin06@bluewin.ch>
2024-09-19 10:56:44 +0200

    linter fixed

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ console.log(chalk.blue("it works"));
 var myUnsignedVariable = 5;
 
 function checkEquality(a, b) {
-    if (a == b) {
+    if (a === b) {
       console.log('Values are equal');
     } else {
       console.log('Values are not equal');
     }
   }
   
+
+myUnsignedVariable++;
+checkEquality(5,myUnsignedVariable)
